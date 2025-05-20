3e1ed8a3334eb3fb247d64075865c13a75471a2c
Vinluvy <123059312+VinLvy@users.noreply.github.com>
2025-05-20 09:50:21 +0700

    API

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function __invoke(): JsonResponse
+    public function __invoke(Request $request)
     {
-        return response()->json(['pesan' => 'Test REST API Controller Laravel 12']);
+        return response()->json([
+            'pesan' => 'API dari Laravel berhasil diakses!'
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TestController;
 
-Route::get('/tes', TestController::class);
+Route::get('/tes', [TestController::class, '__invoke']);
 
 Route::get('/cek', function () {
     return response()->json(['cek' => 'berhasil']);
