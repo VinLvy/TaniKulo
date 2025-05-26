05ec55dbeaaa27e88210c12ea362dddfd3857455
Vinluvy <123059312+VinLvy@users.noreply.github.com>
2025-05-26 10:01:47 +0700

    Models

---
+++ app/Models/Device.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Device extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'name', 'serial_number', 'ssid', 'password', 'is_active'];
+
+    public function moistureReadings()
+    {
+        return $this->hasMany(MoistureReading::class);
+    }
+
+    public function humidityReadings()
+    {
+        return $this->hasMany(HumidityReading::class);
+    }
+
+    public function luxReadings()
+    {
+        return $this->hasMany(LuxReading::class);
+    }
+
+    public function PhReadings()
+    {
+        return $this->hasMany(PhReading::class);
+    }
+}
--- app/Models/LuxReading.php
+++ app/Models/LuxReading.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class HumidityReading extends Model
+class LuxReading extends Model
 {
     use HasFactory;
 
