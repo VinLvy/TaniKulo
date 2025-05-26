290701be7487f1212618236e98b72fd3ac1b6e53
Vinluvy <123059312+VinLvy@users.noreply.github.com>
2025-05-26 10:15:58 +0700

    API 1.4

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Api\TestController;
 use App\Http\Controllers\Api\Auth\GoogleAuthController;
 use App\Http\Controllers\Api\TankLevelsController;
+use App\Http\Controllers\Api\DeviceDataController;
 
 // Tes routes
 Route::match(['get', 'post'], '/tes', [TestController::class, '__invoke']);
@@ -17,14 +18,20 @@ Route::post('/kirim-data', fn (Request $request) => response()->json([
 // Authenticated user route
 Route::middleware('auth:sanctum')->get('/user', fn (Request $request) => $request->user());
 
+// Group: Auth Google
+Route::prefix('auth/google')->controller(GoogleAuthController::class)->group(function () {
+    Route::get('/', 'redirect');         // GET /auth/google
+    Route::get('/callback', 'callback'); // GET /auth/google/callback
+});
+
 // Group: Tank Levels
 Route::prefix('tank-levels')->controller(TankLevelsController::class)->group(function () {
     Route::get('/', 'index');     // GET /tank-levels
     Route::post('/', 'update');   // POST /tank-levels
 });
 
-// Group: Auth Google
-Route::prefix('auth/google')->controller(GoogleAuthController::class)->group(function () {
-    Route::get('/', 'redirect');         // GET /auth/google
-    Route::get('/callback', 'callback'); // GET /auth/google/callback
+// Group: Device Data
+Route::prefix('devices')->group(function () {
+    Route::get('/{serial}/status', [DeviceDataController::class, 'status']);
+    Route::post('/{serial}/data', [DeviceDataController::class, 'sendData']);
 });
