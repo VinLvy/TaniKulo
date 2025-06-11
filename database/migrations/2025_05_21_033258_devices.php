cdca96aeea3c2cc864d317012fee94f86377e421
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2025-06-11 13:52:21 +0700

    gugugaga

--- database/migrations/2025_05_21_033258_devices.php
+++ database/migrations/2025_05_21_033258_devices.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -27,8 +28,17 @@ return new class extends Migration
 
         Schema::create('moisture_readings', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('device_id')->constrained('devices')->onDelete('cascade');
-            $table->float('value');
+            $table->float('moisture');
+            $table->string('status');
+            $table->timestamp('recorded_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+
+        Schema::create('moisture_settings', function (Blueprint $table) {
+            $table->id();
+            $table->float('warnLower');
+            $table->float('warnUpper');
+            $table->string('status');
+            $table->string('set_by');
             $table->timestamp('recorded_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
 
