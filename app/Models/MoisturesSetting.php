266be5844796785f2195de59b69fc7d912bf5721
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2025-06-11 15:33:53 +0700

    creating controller, route, model of setting moisture

---
+++ app/Models/MoisturesSetting.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class MoisturesSetting extends Model
+{
+    use HasFactory;
+
+    protected $table = 'moisture_settings';
+
+    protected $fillable = ['warnLower', 'warnUpper', 'status', 'set_by', 'recorded_at'];
+
+    public $timestamps = false;
+
+    protected $dates = ['recorded_at'];
+
+    public function device()
+    {
+        return $this->belongsTo(Device::class);
+    }
+}
