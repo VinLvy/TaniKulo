2724b7d0690e3548e1aa3b51af9fd54712814efa
Vinluvy <123059312+VinLvy@users.noreply.github.com>
2025-06-12 19:42:00 +0700

    Models 1.3

---
+++ app/Models/HumiditySetting.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class HumiditySetting extends Model
+{
+    use HasFactory;
+
+    protected $table = 'humidity_settings';
+
+    protected $fillable = ['warnLower', 'warnUpper', 'status', 'set_by', 'recorded_at'];
+
+    public $timestamps = false;
+
+    protected $dates = ['recorded_at'];
+
+    public function device()
+    {
+        return $this->belongsTo(Device::class);
+    }
+}
